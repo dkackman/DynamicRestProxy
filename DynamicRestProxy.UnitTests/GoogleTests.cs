a51b78fdbea294ac82edbc580bbaf0689b78a82a
dkackman <dkackman@gmail.com>
2014-04-19 10:39:04 -0500

    missed new files

---
+++ DynamicRestProxy.UnitTests/GoogleTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Dynamic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+
+using RestSharp;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DynamicRestProxy.UnitTests
+{
+    [TestClass]
+    public class GoogleTests
+    {
+        private static dynamic GetGoogleKey()
+        {
+            return APIKEY.JsonKey("google").installed;
+        }
+
+        [TestMethod]
+       // [Ignore] // - this test requires user interaction
+        public async Task DeviceAuthentication()
+        {
+            dynamic key = GetGoogleKey();
+
+            var client = new RestClient("https://accounts.google.com");
+            dynamic proxy = new RestProxy(client);
+            var response = await proxy.o.oauth2.device.code.post(client_id: key.client_id, scope: "email profile");
+            Assert.IsNotNull(response);
+
+            Debug.WriteLine((string)response.user_code);
+
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = string.Format("/c echo {0} | clip", response.user_code);
+            p.Start();
+
+            // this requires user permission - open a broswer - enter the user_code which is now in the clipboard
+            Process.Start((string)response.verification_url);
+
+            int expiration = response.expires_in;
+            int interval = response.interval;
+            int time = interval;
+
+            // we are using the device flow so enter the code in the browser - poll google for success
+            string token = null;
+            while (time < expiration && string.IsNullOrEmpty(token))
+            {
+                Thread.Sleep(interval * 1000);
+                var tokenResonse = await proxy.o.oauth2.token.post(client_id: key.client_id, client_secret: key.client_secret, code:response.device_code, grant_type:"http://oauth.net/grant_type/device/1.0");
+                token = tokenResonse.access_token;
+                time += interval;
+            }
+
+            Assert.IsNotNull(token);
+
+            var api = new RestClient("https://www.googleapis.com");
+            api.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator(token);
+            dynamic apiProxy = new RestProxy(api);
+            var profile = await apiProxy.oauth2.v1.userinfo();
+
+            Assert.IsNotNull(profile);
+            Assert.AreEqual((string)profile.family_name, "Kackman");
+        }
+    }
+}
---
+++ DynamicRestProxy/RequestBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Diagnostics;
+using System.Dynamic;
+
+using RestSharp;
+
+namespace DynamicRestProxy
+{
+    class RequestBuilder
+    {
+        RestProxy _proxy;
+        public RequestBuilder(RestProxy proxy)
+        {
+            Debug.Assert(proxy != null);
+            _proxy = proxy;
+        }
+
+        public RestRequest BuildRequest(InvokeMemberBinder binder, object[] args)
+        {
+            //unnamed arguments are treated as url segments; named arguments are parameters
+            int unnamedArgCount = binder.CallInfo.ArgumentCount - binder.CallInfo.ArgumentNames.Count;
+            // total number of segments are all of the parent segments plus the number of unnamed arguments
+            int parentCount = _proxy.Index + 1;
+            int count = parentCount + unnamedArgCount;
+            string template = CreateUrlSegmentTemplate(count + binder.UrlSegmentOffset());
+
+            var request = new RestRequest(template);
+
+            // if the binder endpoint isn't a verb (post, get etc) it represents a segment of the url - add it
+            if (!binder.IsVerb())
+                request.AddUrlSegment(parentCount.ToString(), binder.Name.TrimStart(_proxy.KeywordEscapeCharacter));
+
+            // fill in the url segments 
+            SetSegments(request, parentCount, count, args);
+
+            // now add all named arguments as parameters
+            SetParameters(request, binder.CallInfo, args, unnamedArgCount);
+
+            return request;
+        }
+
+        private void SetSegments(RestRequest request, int start, int count, object[] args)
+        {
+            // fill in the url segments for this object and its parents
+            _proxy.AddSegment(request);
+
+            // fill in the url segments passed as unnamed arguments to the dynamic invocation
+            for (int i = start; i < count; i++)
+            {
+                request.AddUrlSegment((i + 1).ToString(), args[i - start].ToString());
+            }
+        }
+
+        private void SetParameters(RestRequest request, CallInfo call, object[] args, int offset)
+        {
+            for (int i = 0; i < call.ArgumentNames.Count; i++)
+            {
+                request.AddParameter(call.ArgumentNames[i].TrimStart(_proxy.KeywordEscapeCharacter), args[i + offset]);
+            }
+        }
+
+        private static string CreateUrlSegmentTemplate(int count)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                builder.Append("/{").Append(i).Append("}");
+            }
+            return builder.ToString();
+        }
+    }
+}
