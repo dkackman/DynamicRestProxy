8326949dd1292396628d242fc228e062cbcae623
dkackman <dkackman@gmail.com>
2014-06-20 21:33:45 -0500

    switch unit test to use client instead of proxy

--- DynamicRestPRoxy.Portable.UnitTests/DynamicClientTests.cs
+++ DynamicRestPRoxy.Portable.UnitTests/DynamicClientTests.cs
@@ -20,29 +20,18 @@ namespace DynamicRestProxy.PortableHttpClient.UnitTests
         [TestCategory("integration")]
         public async Task CoordinateFromPostalCode()
         {
-            var handler = new HttpClientHandler();
-            if (handler.SupportsAutomaticDecompression)
-            {
-                handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-            }
+            dynamic client = new DynamicRestClient("http://dev.virtualearth.net/REST/v1/");
 
-            using (var client = new HttpClient(handler, true))
-            {
-                client.BaseAddress = new Uri("http://dev.virtualearth.net/REST/v1/");
+            dynamic result = await client.Locations.get(postalCode: "55116", countryRegion: "US", key: CredentialStore.Key("bing"));
 
-                string key = CredentialStore.Key("bing");
+            Assert.AreEqual((int)result.statusCode, 200);
+            Assert.IsTrue(result.resourceSets.Count > 0);
+            Assert.IsTrue(result.resourceSets[0].resources.Count > 0);
 
-                dynamic proxy = new HttpClientProxy(client);
-                var result = await proxy.Locations.get(postalCode: "55116", countryRegion: "US", key: key);
-
-                Assert.AreEqual((int)result.statusCode, 200);
-                Assert.IsTrue(result.resourceSets.Count > 0);
-                Assert.IsTrue(result.resourceSets[0].resources.Count > 0);
-
-                var r = result.resourceSets[0].resources[0].point.coordinates;
-                Assert.IsTrue((44.9108238220215).AboutEqual((double)r[0]));
-                Assert.IsTrue((-93.1702041625977).AboutEqual((double)r[1]));
-            }
+            var r = result.resourceSets[0].resources[0].point.coordinates;
+            Assert.IsTrue((44.9108238220215).AboutEqual((double)r[0]));
+            Assert.IsTrue((-93.1702041625977).AboutEqual((double)r[1]));
         }
     }
 }
+
