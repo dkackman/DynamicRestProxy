eed3a946f4c97d6fc03b0fa63456e1060f918175
dkackman <dkackman@gmail.com>
2014-06-21 21:37:10 -0500

    client v1.0.2 build

--- DynamicRestProxy.Portable/Properties/AssemblyInfo.cs
+++ DynamicRestProxy.Portable/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DynamicRestProxy.Portable")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Portable dynamic Rest proxy and client")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DynamicRestProxy.Portable")]
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 //[assembly: System.CLSCompliant(true)]
