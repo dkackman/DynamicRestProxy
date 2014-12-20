eadc9815412c91936a1b814280975eef0ed17d96
Don Kackman <dkackman@gmail.com>
2014-12-20 13:25:53 -0600

    revert change that removed SL WP 8.1

--- DynamicRestProxy.Portable/FrameworkTools.cs
+++ DynamicRestProxy.Portable/FrameworkTools.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 using System.Dynamic;
-using System.Linq.Expressions;
 
 namespace DynamicRestProxy.PortableHttpClient
 {
