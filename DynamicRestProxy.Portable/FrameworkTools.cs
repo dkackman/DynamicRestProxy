64162a1022ef479a8ac45556ccb5dad021b47d55
Don Kackman <dkackman@gmail.com>
2014-12-21 10:18:28 -0600

    commenting

--- DynamicRestProxy.Portable/FrameworkTools.cs
+++ DynamicRestProxy.Portable/FrameworkTools.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Reflection;
 using System.Dynamic;
 using System.Diagnostics;
@@ -9,28 +8,33 @@ namespace DynamicRestProxy
 {
     static class FrameworkTools
     {
-        private static FieldInfo _genericTypeArgumentsField;
+        // this is private field of the InvokeMemberBinder base class that is used to access
+        // the generic type arguments
+        private static FieldInfo _typeArgumentsField;
 
         /// <summary>Extension method allowing to easyly extract generic type arguments from <see cref="InvokeMemberBinder"/>.</summary>
         /// <param name="binder">Binder from which get type arguments.</param>
         /// <returns>List of types passed as generic parameters.</returns>
         public static IEnumerable<Type> GetGenericTypeArguments(this InvokeMemberBinder binder)
         {
-            if (_genericTypeArgumentsField == null)
+            // if we haven't cached the private field of the base class do so now
+            if (_typeArgumentsField == null)
             {
+                // using reflection get the FieldInfo of the private typeArguments field
+                // mono and MS .net use different naming for this field
                 string fieldName = Type.GetType("Mono.Runtime") != null ? "typeArguments" : "m_typeArguments";
-                _genericTypeArgumentsField = binder.GetType().GetTypeInfo().GetDeclaredField(fieldName);
+                _typeArgumentsField = binder.GetType().GetTypeInfo().GetDeclaredField(fieldName);
             }
 
-            if (_genericTypeArgumentsField != null)
+            // if the field info is still null, something changed in how .net implements the dynamic binder
+            if (_typeArgumentsField != null)
             {
-                return _genericTypeArgumentsField.GetValue(binder) as IEnumerable<Type> ?? new List<Type>();
+                return _typeArgumentsField.GetValue(binder) as IEnumerable<Type> ?? new List<Type>();
             }
 
             Debug.Assert(false, "Retrieving the private collection of generic type arguments failed");
             // Sadly return empty collection if failed.
             return new List<Type>();
         }
-
     }
 }
