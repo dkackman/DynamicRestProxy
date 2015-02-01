1d3974ed863482794751b9b249e9c885f739ac7d
Don Kackman <dkackman@gmail.com>
2015-02-01 13:11:20 -0600

    minor code removal

--- DynamicRestProxy.Portable/RequestBuilder.cs
+++ DynamicRestProxy.Portable/RequestBuilder.cs
@@ -25,16 +25,12 @@ namespace DynamicRestProxy.PortableHttpClient
             Debug.Assert(_methods.ContainsKey(verb), "unrecognized verb. check the BinderExtensions _verbs array");
 
             var method = _methods[verb];
-            var request = new HttpRequestMessage()
+            return new HttpRequestMessage()
             {
                 Method = method,
-                RequestUri = MakeUri(method, namedArgs)
+                RequestUri = MakeUri(method, namedArgs),
+                Content = CreateContent(method, unnamedArgs, namedArgs)
             };
-
-            // create content object as appropriate to the args passed and method
-            request.Content = CreateContent(method, unnamedArgs, namedArgs);
-
-            return request;
         }
 
         private Uri MakeUri(HttpMethod method, IEnumerable<KeyValuePair<string, object>> namedArgs)
