14c07ece0c994e76301da6f7064b0c4cff0559b0
Don Kackman <dkackman@gmail.com>
2015-08-24 17:19:13 -0500

    update FakeHttp

---
+++ Universal.UnitTests/BasicUniversalTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+using DynamicRestProxy.PortableHttpClient;
+
+using Windows.Storage;
+using Windows.ApplicationModel;
+
+using FakeHttp;
+
+namespace Universal.UnitTests
+{
+    [TestClass]
+    public class BasicFunctionalityTests
+    {
+        //[TestMethod]
+        //[TestCategory("Universal")]
+        //public async Task CanGetStorageFile()
+        //{
+        //    await DumpFolder(Package.Current.InstalledLocation);
+
+
+        //}
+
+        //private async static Task DumpFolder(StorageFolder folder)
+        //{
+        //    Debug.WriteLine(folder.Name);
+
+        //    foreach (var file in await folder.GetFilesAsync())
+        //    {
+        //        Debug.WriteLine("\t" + file.Name);
+        //    }
+
+        //    foreach (var sub in await folder.GetFoldersAsync())
+        //    {
+        //        await DumpFolder(sub);
+        //    }
+        //}
+
+        [TestMethod]
+        [TestCategory("Universal")]
+        public async Task GetPublicBucket()
+        {
+            var responses = await Package.Current.InstalledLocation.GetFolderAsync("MockResponses");
+            var store = new StorageFolderResponseStore(responses);
+            using (var handler = new FakeHttpMessageHandler(store))
+            using (dynamic google = new DynamicRestClient("https://www.googleapis.com/", handler))
+            {
+                dynamic bucket = google.storage.v1.b("uspto-pair");
+
+                dynamic metaData = await bucket.get();
+                Assert.IsNotNull(metaData);
+
+                dynamic objects = await bucket.o.get();
+                Assert.IsNotNull(objects);
+                Assert.IsTrue(objects.items.Count > 0);
+            }
+        }
+    }
+}
