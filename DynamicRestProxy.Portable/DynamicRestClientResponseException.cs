79fbcd8ec3e30ebe01a43256acdeeed48703bdfb
Don Kackman <dkackman@gmail.com>
2015-09-09 22:48:33 -0500

    add comenting, additionla constructor and null conditional operator in case response is null

--- DynamicRestProxy.Portable/DynamicRestClientResponseException.cs
+++ DynamicRestProxy.Portable/DynamicRestClientResponseException.cs
@@ -3,11 +3,34 @@ using System.Net.Http;
 
 namespace DynamicRestProxy.PortableHttpClient
 {
+    /// <summary>
+    /// Exception thrown when response status does not indicate success (<see cref="HttpResponseMessage.IsSuccessStatusCode"/>)
+    /// Allows response content and headers to be inspected on failure
+    /// </summary>
     public class DynamicRestClientResponseException : Exception
     {
+        /// <summary>
+        /// The response
+        /// </summary>
         public HttpResponseMessage Response { get; private set; }
 
-        public DynamicRestClientResponseException(HttpResponseMessage response) : base(response.ReasonPhrase)
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="response">The response</param>
+        public DynamicRestClientResponseException(HttpResponseMessage response) 
+            : base(response?.ReasonPhrase)
+        {
+            Response = response;
+        }
+
+        /// <summary>
+        /// ctore
+        /// </summary>
+        /// <param name="response">The response</param>
+        /// <param name="inner">An inner exception</param>
+        public DynamicRestClientResponseException(HttpResponseMessage response, Exception inner) 
+            : base(response?.ReasonPhrase, inner)
         {
             Response = response;
         }
